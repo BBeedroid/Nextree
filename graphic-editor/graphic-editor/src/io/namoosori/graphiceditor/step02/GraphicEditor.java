35efec32c8577eff26c5493a82cb90f4eef339ab
BBeedroid <maisy13@naver.com>
2023-11-16 12:30:41 +0900

    feat: Graphic Editor 프로그래밍을 통한 상속과 오버라이딩 개념 학습

---
+++ graphic-editor/graphic-editor/src/io/namoosori/graphiceditor/step02/GraphicEditor.java
@@ -0,0 +1,34 @@
+package io.namoosori.graphiceditor.step02;
+
+public class GraphicEditor {
+    private static final int ARRAY_LENGTH = 5;
+    public static void main(String[] args) {
+        Shape[] shapes = new Shape[ARRAY_LENGTH];
+
+        // Generate Shapes
+        for (int i = 0; i < ARRAY_LENGTH; i++) {
+            int randomNumber = (int)(Math.random() * 3);
+
+            if (randomNumber == 0) {
+                shapes[i] = new Rectangle();
+            } else if (randomNumber == 1) {
+                shapes[i] = new Ellipse();
+            } else {
+                shapes[i] = new Line();
+            }
+        }
+
+        //Drawing Shapes
+        for (Shape shape : shapes) {
+            if (shape instanceof Rectangle) {
+//                Rectangle rect = (Rectangle) shape;
+//                rect.drawRectangle();
+                ((Rectangle)shape).drawRectangle();
+            } else if (shape instanceof Ellipse) {
+                ((Ellipse)shape).drawEllipse();
+            } else if (shape instanceof Line) {
+                ((Line)shape).drawLine();
+            }
+        }
+    }
+}
