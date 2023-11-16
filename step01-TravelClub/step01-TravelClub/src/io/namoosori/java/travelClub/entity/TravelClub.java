67d6e7291618225d166847c7e06324cd8cb88ee4
BBeedroid <maisy13@naver.com>
2023-11-16 12:33:16 +0900

    feat: TravelClub UI 생성

---
+++ step01-TravelClub/step01-TravelClub/src/io/namoosori/java/travelClub/entity/TravelClub.java
@@ -0,0 +1,83 @@
+package io.namoosori.java.travelClub.entity;
+
+import io.namoosori.java.travelClub.util.DateUtil;
+
+import java.sql.SQLOutput;
+import java.util.UUID;
+
+public class TravelClub {
+    private static final int MINIMUM_NAME_LENGTH = 3;
+    private static final int MINIMUM_INTRO_LENGTH = 5;
+
+    //Fields
+    private String id;
+    private String clubName;
+    private String intro;
+    private String foundationDay;
+
+    private TravelClub() {
+        this.id = UUID.randomUUID().toString();
+    }
+
+    public TravelClub(String clubName, String intro) {
+        this();
+        setClubName(clubName);
+        setIntro(intro);
+        this.foundationDay = DateUtil.today();
+    }
+
+    //Methods
+    //setter
+    public void setClubName(String clubName) {
+        if (clubName.length() < MINIMUM_NAME_LENGTH) {
+            System.out.println("Club name should be longer than 3");
+            return;
+        }
+        this.clubName = clubName;
+    }
+
+    //getter
+    public String getClubName() {
+        return this.clubName;
+    }
+
+    public void setIntro(String intro) {
+        if (intro.length() < MINIMUM_INTRO_LENGTH) {
+            System.out.println("Club intro should be longer than " + MINIMUM_INTRO_LENGTH);
+            return;
+        }
+        this.intro = intro;
+    }
+
+    public String getIntro() {
+        return this.intro;
+    }
+
+    public String getId() {
+        return this.id;
+    }
+
+    public String getFoundationDay() {
+        return this.foundationDay;
+    }
+
+    public static TravelClub getSample() {
+        String clubName = "Sample Club";
+        String intro = "Sample Club Intro";
+        TravelClub sampleClub = new TravelClub(clubName, intro);
+        return sampleClub;
+
+        //return new TravelClub("Sample Club", "Sample Club Intro");
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder builder = new StringBuilder();
+        builder.append("TravelCulb club ID : ").append(id);
+        builder.append(", ClubName : ").append(clubName);
+        builder.append(", Intro : ").append(intro);
+        builder.append(", FoundationDay : ").append(foundationDay);
+
+        return builder.toString();
+    }
+}
---
+++ step01-TravelClub/step01-TravelClub/src/io/namoosori/java/travelClub/ui/console/ClubConsole.java
@@ -0,0 +1,149 @@
+package io.namoosori.java.travelClub.ui.console;
+
+import io.namoosori.java.travelClub.entity.TravelClub;
+import io.namoosori.java.travelClub.service.ClubService;
+import io.namoosori.java.travelClub.service.ServiceLogicLifeCycler;
+import io.namoosori.java.travelClub.util.ConsoleUtil;
+
+public class ClubConsole {
+    private ConsoleUtil consoleUtil;
+    private ClubService clubService;
+
+    public ClubConsole() {
+        this.consoleUtil = new ConsoleUtil();
+        this.clubService = ServiceLogicLifeCycler.getUniqueInstance().getClubService();
+    }
+
+    public void register() {
+        //
+        while (true) {
+            String clubName = consoleUtil.getValueOf("Club Name(0.Club menu)");
+            if (clubName.equals("0")) {
+                return;
+            }
+
+            String intro = consoleUtil.getValueOf("Club Intro(0.Club menu)");
+            if (intro.equals("0")) {
+                return;
+            }
+
+            TravelClub newClub = new TravelClub(clubName, intro);
+
+            clubService.register(newClub);
+
+            System.out.println("Registered club : " + newClub);
+        }
+    }
+
+    public void findAll() {
+        TravelClub[] foundClubs = clubService.findAll();
+
+        if (foundClubs.length == 0) {
+            System.out.println("Empty");
+        }
+
+        for (TravelClub club : foundClubs) {
+            System.out.println(club.toString());
+        }
+    }
+
+    public void findById() {
+        TravelClub foundClub = null;
+
+        while (true) {
+            String clubId = consoleUtil.getValueOf("Club id to find(0.Club Menu)");
+            if (clubId.equals("0")) {
+                break;
+            }
+
+            foundClub = clubService.findById(clubId);
+
+            if (foundClub != null) {
+                System.out.println(foundClub);
+            } else {
+                System.out.println("Can not find club, ID : " + clubId);
+            }
+        }
+    }
+
+    public void findByName() {
+        TravelClub[] foundClubs = null;
+
+        while (true) {
+            String clubName = consoleUtil.getValueOf("Club name to find(0.Club Menu)");
+            if (clubName.equals("0")) {
+                break;
+            }
+
+            foundClubs = clubService.findByName(clubName);
+
+            if (foundClubs != null && foundClubs.length != 0) {
+                for (TravelClub club : foundClubs) {
+                    System.out.println(club);
+                }
+            } else {
+                System.out.println("Can not find club, name : " + clubName);
+            }
+        }
+    }
+
+    private TravelClub findOne() {
+        TravelClub foundClub = null;
+
+        while (true) {
+            String clubId = consoleUtil.getValueOf("Club id to find(0.Club Menu)");
+            if (clubId.equals("0")) {
+                break;
+            }
+
+            foundClub = clubService.findById(clubId);
+
+            if (foundClub != null) {
+                break;
+            } else {
+                System.out.println("Can not find club, ID : " + clubId);
+            }
+        }
+
+        return foundClub;
+    }
+
+    public void modify() {
+        TravelClub targetClub = findOne();
+        if (targetClub == null) {
+            return;
+        }
+
+        String newName = consoleUtil.getValueOf("New Club Name(0.Club Menu, Enter. No Change)");
+        if (newName.equals("0")) {
+            return;
+        }
+        if (!newName.isEmpty()) {
+            targetClub.setClubName(newName);
+        }
+
+        String newIntro = consoleUtil.getValueOf("New Club Intro(0.Club Menu, Enter. No Change)");
+        if (!newIntro.isEmpty()) {
+            targetClub.setIntro(newIntro);
+        }
+
+        clubService.modify(targetClub);
+
+        System.out.println("Modified club : " + targetClub);
+    }
+
+    public void remove() {
+        TravelClub targetClub = findOne();
+        if (targetClub == null) {
+            return;
+        }
+
+        String confirmStr = consoleUtil.getValueOf("Remove this club? (Y: yes, N: no)");
+        if (confirmStr.toLowerCase().equals("y") || confirmStr.toLowerCase().equals("yes")) {
+            System.out.println("Removing the club : " + targetClub.getClubName());
+            clubService.remove(targetClub.getId());
+        } else {
+            System.out.println("Cancelled remove, your club is safe : " + targetClub.getClubName());
+        }
+    }
+}
