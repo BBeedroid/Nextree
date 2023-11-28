c76157bf45c18fffb13eef7dab43b3be7b31fc4e
BBeedroid <maisy13@naver.com>
2023-11-28 16:41:56 +0900

    feat: Travel Club Entities

---
+++ backend/src/main/java/com/example/backend/entity/Board.java
@@ -0,0 +1,45 @@
+package com.example.backend.entity;
+
+import com.example.backend.dto.BoardDTO;
+import com.example.backend.uils.BaseTimeEntity;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "BOARD")
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Board extends BaseTimeEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "BOARD_ID", nullable = false)
+    private long boardId;
+
+    @Column(name = "BOARD_TITLE", nullable = false)
+    @Size(min = 3)
+    private String boardTitle;
+
+    @Column(name = "BOARD_INTRO")
+    private String boardIntro;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "CLUB_ID")
+    private Club club;
+
+    public BoardDTO EntityToDTO() {
+        return BoardDTO.builder()
+                .boardId(this.boardId)
+                .boardTitle(this.boardTitle)
+                .boardIntro(this.boardIntro)
+                .createdTime(this.getCreatedTime())
+                .updatedTime(this.getUpdatedTime())
+                .clubId(this.club.getClubId())
+                .build();
+    }
+}
