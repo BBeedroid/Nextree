3914539697fd8238b72b11996eacc7de2668233b
BBeedroid <maisy13@naver.com>
2023-12-01 08:43:56 +0900

    feat: Board 게시판 제목 검색, 클럽 내 게시판 리스트 검색 기능 구현

--- backend/src/main/java/com/example/backend/controller/BoardController.java
+++ backend/src/main/java/com/example/backend/controller/BoardController.java
@@ -3,7 +3,9 @@ package com.example.backend.controller;
 import com.example.backend.dto.BoardDTO;
 import com.example.backend.dto.ResponseDTO;
 import com.example.backend.service.BoardService;
+import com.example.backend.util.BoardDuplicationException;
 import com.example.backend.util.NoPermissionToCreateBoard;
+import com.example.backend.util.NoSuchBoardException;
 import com.example.backend.util.NoSuchClubException;
 import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
@@ -12,6 +14,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.naming.AuthenticationException;
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -33,8 +36,54 @@ public class BoardController {
             boardService.register(clubId, boardDTO, currentUserId);
             responseDTO.setItem(boardDTO);
             responseDTO.setStatusCode(HttpStatus.OK.value());
-            return ResponseEntity.ok(boardDTO);
-        } catch (NoSuchClubException | NoPermissionToCreateBoard | AuthenticationException e) {
+            return ResponseEntity.ok(responseDTO);
+        } catch (NoSuchClubException | BoardDuplicationException | NoPermissionToCreateBoard | AuthenticationException e) {
+            responseDTO.setErrorMessage(e.getMessage());
+            responseDTO.setStatusCode(HttpStatus.BAD_REQUEST.value());
+            return ResponseEntity.badRequest().body(responseDTO);
+        }
+    }
+
+    @GetMapping("/{boardId}")
+    public ResponseEntity<?> searchBoard(@PathVariable Long boardId) {
+        ResponseDTO<BoardDTO> responseDTO = new ResponseDTO<>();
+        try {
+            BoardDTO boardDTO = boardService.findBoard(boardId);
+            responseDTO.setItem(boardDTO);
+            responseDTO.setStatusCode(HttpStatus.OK.value());
+            return ResponseEntity.ok(responseDTO);
+        } catch (NoSuchBoardException e) {
+            responseDTO.setErrorMessage(e.getMessage());
+            responseDTO.setStatusCode(HttpStatus.BAD_REQUEST.value());
+            return ResponseEntity.badRequest().body(responseDTO);
+        }
+    }
+
+    @GetMapping
+    public ResponseEntity<?> searchBoardByTitle(@RequestParam Long clubId,
+                                                @RequestParam String boardTitle) {
+        ResponseDTO<BoardDTO> responseDTO = new ResponseDTO<>();
+        try {
+            BoardDTO boardDTO = boardService.findByClubIdAndBoardTitle(clubId, boardTitle);
+            responseDTO.setItem(boardDTO);
+            responseDTO.setStatusCode(HttpStatus.OK.value());
+            return ResponseEntity.ok(responseDTO);
+        } catch (NoSuchBoardException e) {
+            responseDTO.setErrorMessage(e.getMessage());
+            responseDTO.setStatusCode(HttpStatus.BAD_REQUEST.value());
+            return ResponseEntity.badRequest().body(responseDTO);
+        }
+    }
+
+    @GetMapping("/list")
+    public ResponseEntity<?> searchBoardByClubId(@RequestParam Long clubId) {
+        ResponseDTO<BoardDTO> responseDTO = new ResponseDTO<>();
+        try {
+            List<BoardDTO> foundBoards = boardService.findByClubId(clubId);
+            responseDTO.setItems(foundBoards);
+            responseDTO.setStatusCode(HttpStatus.OK.value());
+            return ResponseEntity.ok().body(responseDTO);
+        } catch (NoSuchClubException e) {
             responseDTO.setErrorMessage(e.getMessage());
             responseDTO.setStatusCode(HttpStatus.BAD_REQUEST.value());
             return ResponseEntity.badRequest().body(responseDTO);
