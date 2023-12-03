2144d530624cf3140bdd2afbc59f9c11fd249f83
BBeedroid <maisy13@naver.com>
2023-12-03 13:59:59 +0900

    feat: 회원가입 페이지 생성 및 백엔드 연결, 로그인&회원가입 페이지 연결

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Login from "./pages/Login/Login";
+import SignUp from "./pages/SignUp/SignUp";
 
 function App(): JSX.Element {
     return (
         <Routes>
             <Route path="/" element={<Login />} />
+            <Route path="/signup" element={<SignUp />} />
         </Routes>
     );
 }
