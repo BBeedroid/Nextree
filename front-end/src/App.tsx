63fc6237d8f603da8ae6f60340e114b4f8c15272
BBeedroid <maisy13@naver.com>
2023-12-04 14:18:46 +0900

    feat: 가입한 클럽 목록 조회 페이지 생성 및 기능 구현

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -2,12 +2,14 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Login from "./pages/Login/Login";
 import SignUp from "./pages/SignUp/SignUp";
+import ClubList from "./pages/Club/ClubList";
 
 function App(): JSX.Element {
     return (
         <Routes>
             <Route path="/" element={<Login />} />
             <Route path="/signup" element={<SignUp />} />
+            <Route path="/my-club-list" element={<ClubList />} />
         </Routes>
     );
 }
