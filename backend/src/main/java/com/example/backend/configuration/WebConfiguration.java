7a9b678ef4a183fd9012753eec74abbf385ea68f
BBeedroid <maisy13@naver.com>
2023-12-04 11:13:41 +0900

    feat: JWT 토큰 인터셉터 생성 및 SpringMVC 적용

--- backend/src/main/java/com/example/backend/configuration/WebConfiguration.java
+++ backend/src/main/java/com/example/backend/configuration/WebConfiguration.java
@@ -1,11 +1,17 @@
 package com.example.backend.configuration;
 
+import com.example.backend.jwt.JwtTokenInterceptor;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@RequiredArgsConstructor
 public class WebConfiguration implements WebMvcConfigurer {
+    private final JwtTokenInterceptor jwtTokenInterceptor;
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
@@ -13,5 +19,12 @@ public class WebConfiguration implements WebMvcConfigurer {
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*")
                 .allowCredentials(true);
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(jwtTokenInterceptor)
+                .addPathPatterns(("/api/**"))
+                .excludePathPatterns("/api/member/signup");
     }
 }
---
+++ backend/src/main/java/com/example/backend/jwt/JwtTokenInterceptor.java
@@ -0,0 +1,27 @@
+package com.example.backend.jwt;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+@Component
+@RequiredArgsConstructor
+public class JwtTokenInterceptor implements HandlerInterceptor {
+    private final JwtTokenProvider jwtTokenProvider;
+
+    @Override
+    public boolean preHandle(HttpServletRequest request,
+                             HttpServletResponse response,
+                             Object handler)
+            throws Exception {
+        String token = jwtTokenProvider.resolveToken(request);
+        if (token != null && jwtTokenProvider.validationToken(token)) {
+            return true;
+        } else {
+            response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Unauthorized");
+            return false;
+        }
+    }
+}
