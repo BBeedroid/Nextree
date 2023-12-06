71a8bf50e3e9f9e14566b98de6d11bfa66758905
BBeedroid <maisy13@naver.com>
2023-12-06 09:11:46 +0900

    feat: 클럽 게시판 생성 모달 생성 및 기능 구현

--- front-end/src/pages/Board/utils/boardservice.ts
+++ front-end/src/pages/Board/utils/boardservice.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 import { SPRING_API_URL } from "../../../config";
-import { ResponseDTO, BoardDTO, PostDTO } from "../../Util/dtoTypes";
+import { ResponseDTO, BoardDTO, PostDTO, ClubDTO } from "../../Util/dtoTypes";
 
 export const fetchAllBoards = async (clubId: number): Promise<BoardDTO[]> => {
     try {
@@ -40,5 +40,25 @@ export const fetchPostsByBoard = async (
     } catch (error) {
         console.error("게시글 목록을 불러오는 데 실패했습니다.", error);
         throw error;
+    }
+};
+
+export const fetchClub = async (
+    clubId: number,
+): Promise<ClubDTO | undefined> => {
+    try {
+        const response = await axios.get<ResponseDTO<ClubDTO>>(
+            `${SPRING_API_URL}/api/club/${clubId}`,
+            {
+                headers: {
+                    Authorization: `Bearer ${localStorage.getItem("token")}`,
+                },
+            },
+        );
+        console.log(response.data.item);
+        return response.data.item;
+    } catch (error) {
+        console.error("클럽 정보를 불러오는 데 실패했습니다.", error);
+        return undefined;
     }
 };
