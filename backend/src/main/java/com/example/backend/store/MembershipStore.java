1231bb713ee021a319650db56d38c0e309fd1af9
BBeedroid <maisy13@naver.com>
2023-12-06 22:28:31 +0900

    feat: 회원 추방 기능 구현(DB 적용 안 됨)

--- backend/src/main/java/com/example/backend/store/MembershipStore.java
+++ backend/src/main/java/com/example/backend/store/MembershipStore.java
@@ -12,6 +12,10 @@ public interface MembershipStore extends JpaRepository<Membership, Long> {
     Membership findByClub_ClubIdAndMember_MemberId(Long clubId, Long memberId);
 
     @Modifying
-    @Query("delete from Membership m where m.club.clubId = :clubId and m.member.memberId = :memberId")
+    @Query("DELETE FROM Membership m WHERE m.club.clubId = :clubId AND m.member.memberId = :memberId")
     void deleteByClub_IdAndMember_Id(@Param("clubId") Long clubId, @Param("memberId") Long memberId);
+
+    @Modifying
+    @Query("DELETE FROM Membership m WHERE m.club.clubId = :clubId AND m.membershipId = :membershipId")
+    void deleteByCLub_ClubIdAndMembershipId(@Param("clubId") Long clubId, @Param("membershipId") Long membershipId);
 }
